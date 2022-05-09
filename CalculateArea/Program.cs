bddf4077d2e03deb3fc5bfed21ebf0f0c8ca7916
Rikhus <khusainovrik@gmail.com>
2022-05-09 22:17:50 +0300

    Added readme, renamed project

--- CalculateArea/Program.cs
+++ CalculateArea/Program.cs
@@ -7,19 +7,7 @@ namespace CalculateArea
     {
         static void Main(string[] args)
         {
-            Circle circle = new Circle(10);
-            Console.WriteLine($"Circle area: {circle.Area}");
-
-            Triangle triangle1 = new Triangle(12, 13, 10);
-            Triangle triangle3 = new Triangle(15, 12, 9);
-
-            List<Triangle> triangles = new List<Triangle> { triangle1, triangle3 };
-
-            foreach(Triangle triangle in triangles)
-            {
-                Console.WriteLine($"Triangle area: {triangle.Area}, is rectangular: {triangle.IsRectangular}");
-
-            }
+            
         }
     }
 }
